3c5302bea334829d5f2ab32c6271ca4125fc8dd0
Gobika K <gobikakrishna@gmail.com>
2023-08-31 20:33:10 +0200

    added stripe functionality to checkout process for credit card

--- src/app/order-placement/order-placement.component.ts
+++ src/app/order-placement/order-placement.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { orderPlacementService } from './order-placement.service';
 
 @Component({
@@ -9,13 +8,16 @@ import { orderPlacementService } from './order-placement.service';
   styleUrls: ['./order-placement.component.css'],
 })
 export class OrderPlacementComponent implements OnInit {
-  constructor(
-    private shoppinglistService: ShoppingListService,
-    private orderService: orderPlacementService
-  ) {}
+  constructor(public orderService: orderPlacementService) {}
   quantity: number;
   price: number;
   modalOpen = false;
+  paymentMethod: string = 'cd'; // Default payment method
+  buttonLabel: string = 'Place Order'; // Default button label
+  paymentHandler: any = null;
+  stripeAPIKey: any =
+    'pk_test_51Nkop0SD0VVqBFnMDgcoNw8jOhyaeJgUO6WMNEl3dZ5x0RpV3JWzBOLeYj7PzCQ3YObuk15V1p9D1LUvdBoAD5P100gmzdnbE6';
+
   ngOnInit(): void {
     // document.body.style.backgrocsAund = '#f6f6f6'; // display background color only to orders page
     this.orderService.orderValue.subscribe((orderData) => {
@@ -24,11 +26,64 @@ export class OrderPlacementComponent implements OnInit {
       this.price = orderData.price;
       console.log(this.quantity);
     });
+    this.invokeStripe();
   }
 
   orderForm: NgForm;
   onSubmit(form: NgForm) {
     console.log(form.value);
-    this.modalOpen = true;
+    if (this.paymentMethod == 'cd') {
+      this.modalOpen = true;
+    } else if (this.paymentMethod == 'card') {
+      this.makePayment();
+    }
+  }
+  onPaymentMethodChange(method: string) {
+    this.paymentMethod = method;
+
+    if (method === 'cd') {
+      this.buttonLabel = 'Place Order';
+    } else if (method === 'card') {
+      this.buttonLabel = 'Checkout';
+    }
+  }
+  makePayment() {
+    console.log('triggered');
+    const paymentHandler = (<any>window).StripeCheckout.configure({
+      key: this.stripeAPIKey,
+      locale: 'auto',
+      token: function (stripeToken: any) {
+        console.log(stripeToken);
+        this.modalOpen = true;
+      },
+    });
+    paymentHandler.open({
+      name: 'your Shopping',
+      description: 'please enter below details',
+      amount: this.price,
+    });
+  }
+
+  invokeStripe() {
+    if (!window.document.getElementById('stripe-script')) {
+      const script = window.document.createElement('script');
+
+      script.id = 'stripe-script';
+      script.type = 'text/javascript';
+      script.src = 'https://checkout.stripe.com/checkout.js';
+      script.onload = () => {
+        this.paymentHandler = (<any>window).StripeCheckout.configure({
+          key: this.stripeAPIKey,
+          locale: 'auto',
+          token: function (stripeToken: any) {
+            console.log(stripeToken);
+            alert('Payment has been successfull!');
+            this.modalOpen = true;
+          },
+        });
+      };
+
+      window.document.body.appendChild(script);
+    }
   }
 }
